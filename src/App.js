773844b01991788615e021250fbac65be2b9c045
Tim Kozemzak <timkozemzak@gmail.com>
2021-01-18 16:31:53 -0700

    add reminder toggle and AddTask form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from 'react';
 
 
@@ -12,7 +13,18 @@ function App() {
      day: 'Feb 5th at 2:30pm',
      reminder: false,
     }, 
+    {
+     id: 2,
+     text: 'Food eating',
+     day: 'Feb 10th at 6:30pm',
+     reminder: false,
+    }, 
  ])
+
+ //Add task
+ const addTask = (task) => {
+   console.log("task from Addtask:", task);
+ }
 
  //Delete task
  const deleteTask = (id) => {
@@ -20,14 +32,22 @@ function App() {
    setTasks(tasks.filter((task) => task.id !== id))
  }
 
+ //Toggle reminder
+ const toggleReminder = (id) => {
+   setTasks(tasks.map((task) => task.id === id
+   ? { ...task, reminder: !task.reminder } : task))
+ }
+
 
 
   return (
     <div className="container">
         <Header title="Task Tracker"/>
+        <AddTask onAdd={addTask}/>
         {tasks.length > 0 ? 
-        <Tasks tasks={tasks} onDelete={deleteTask}/> : 
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 
         'No Tasks To Show'}
+
     </div>
   );
 }
